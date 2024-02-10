898a51ac6a545df66134a389016886f4280d8ee4
Joshua Ewulo <jewulo@gmail.com>
2024-02-10 20:17:33 +0000

    Not yet implemented.
    See..
    https://www.youtube.com/watch?v=Y2pmZlP-cOU
    https://gist.github.com/JosePaumard/ee935c53f942a25c3cd38027ce4e31c3

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+// https://www.youtube.com/watch?v=Y2pmZlP-cOU
+// https://gist.github.com/JosePaumard/ee935c53f942a25c3cd38027ce4e31c3
+// Data Oriented Programming in Java 21, Solving the Countdown game - JEP Cafe #22
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
